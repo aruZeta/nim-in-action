6b8bb8bcb833cc5f5ff33fd2c8910670a633b6cb
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-06-24 12:39:11 +0200

    [refactor] chapter3/ChatApp/src/client.nim works with the server

--- chapter3/ChatApp/src/client.nim
+++ chapter3/ChatApp/src/client.nim
@@ -1,14 +1,31 @@
 import os
 import threadpool
+import asyncdispatch
+import asyncnet
+import protocol
+
+proc connect(socket: AsyncSocket, serverAddr: string) {.async.} =
+  echo("Connecting to ", serverAddr)
+  await socket.connect(serverAddr, 7687.Port)
+  echo("Connected!")
+
+  while true:
+    let line = await socket.recvLine()
+    let parsed = parseMessage(line)
+    echo(parsed.username, " said ", parsed.message)
 
 echo("ChatApp started")
 
 if paramCount() == 0:
   quit("Please specify the server address, e.g. ./client localhost")
-
 let serverAddr = paramStr(1)
-echo("Connecting to ", serverAddr)
+var socket = newAsyncSocket()
+asyncCheck connect(socket, serverAddr)
 
+var messageFlowVar = spawn stdin.readLine
 while true:
-  let message = spawn stdin.readLine()
-  echo("Sending \"", ^message, "\"")
+  if messageFlowVar.isReady():
+    let message = createMessage("Anonymous", ^messageFlowVar)
+    asyncCheck socket.send(message)
+    messageFlowVar = spawn stdin.readLine()
+  asyncdispatch.poll()
