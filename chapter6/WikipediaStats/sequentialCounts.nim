a6779bee6b49885ee5c787946447dc61353b1903
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-03 12:45:35 +0200

    [add] chapter6/WikipediaStats/sequentialCounts.nim

---
+++ chapter6/WikipediaStats/sequentialCounts.nim
@@ -0,0 +1,37 @@
+import os
+import parseutils
+
+proc parse(line: string,
+           domainCode, pageTitle: var string,
+           countViews, totalSize: var int) =
+  var i = 0
+
+  domainCode.setLen(0)
+  i.inc line.parseUntil(domainCode, {' '}, i)
+  i.inc()
+
+  pageTitle.setLen(0)
+  i.inc line.parseUntil(pageTitle, {' '}, i)
+  i.inc
+
+  countViews = 0
+  i.inc line.parseInt(countViews, i)
+  i.inc
+
+  totalSize = 0
+  i.inc line.parseInt(totalSize, i)
+
+proc readPageCounts(filename: string) =
+  var domainCode, pageTitle: string
+  var countViews, totalSize: int
+  var mostPopular = ("", "", 0, 0)
+  for line in filename.lines:
+    line.parse(domainCode, pageTitle, countViews, totalSize)
+    if domainCode == "en" and countViews > mostPopular[2]:
+      mostPopular = (domainCode, pageTitle, countViews, totalSize)
+  echo("Most popular is: ", mostPopular)
+
+when isMainModule:
+  const file = "pagecounts-20160101-050000"
+  let filename = getCurrentDir() / file
+  readPageCounts(filename)
