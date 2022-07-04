b8bd71c69f4ac3f360c3cd1549fb75ce7e1e8658
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-04 11:37:07 +0200

    [add] chapter6/parallelIncrementerWithChannels.nim

---
+++ chapter6/parallelIncrementerWithChannels.nim
@@ -0,0 +1,20 @@
+import threadpool
+
+var resultChan: Channel[int]
+resultChan.open()
+
+proc increment(x: int) =
+  var counter = 0
+  for i in 0 .. < x:
+    counter.inc()
+  resultChan.send(counter)
+
+spawn increment(10_000)
+spawn increment(10_000)
+sync()
+
+var total = 0
+for i in 0 .. < resultChan.peek:
+  total.inc resultChan.recv()
+
+echo(total)
