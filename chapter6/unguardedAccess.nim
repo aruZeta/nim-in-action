2df64dfa478f4726657c17bc7a6794f2cc7a7ba2
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-04 11:36:28 +0200

    [add] chapter6/unguardedAccess.nim

---
+++ chapter6/unguardedAccess.nim
@@ -0,0 +1,18 @@
+import threadpool
+import locks
+
+var counterLock: Lock
+counterLock.initLock()
+var counter {.guard: counterLock.} = 0
+
+proc increment(x: int) =
+  for i in 0 .. < x:
+    var value = counter
+    value.inc()
+    counter = value
+
+spawn increment(10_000)
+spawn increment(10_000)
+sync()
+
+echo(counter)
