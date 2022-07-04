e7cf05bcb92f50e1dfc91a13d25e0bb0109f2c94
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-04 11:36:15 +0200

    [add] chapter6/raceCondition.nim

---
+++ chapter6/raceCondition.nim
@@ -0,0 +1,15 @@
+import threadpool
+
+var counter = 0
+
+proc increment(x: int) =
+  for i in 0 .. < x:
+    var value = counter
+    value.inc()
+    counter = value
+
+spawn increment(10_000)
+spawn increment(10_000)
+sync()
+
+echo(counter)
