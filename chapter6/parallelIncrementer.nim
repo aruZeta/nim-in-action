d37f1d8a5c55c4247665a6b5efad76127fdbb6dc
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-04 11:36:36 +0200

    [add] chapter6/parallelIncrementer.nim

---
+++ chapter6/parallelIncrementer.nim
@@ -0,0 +1,19 @@
+import threadpool
+import locks
+
+var counterLock: Lock
+counterLock.initLock()
+var counter {.guard: counterLock.} = 0
+
+proc increment(x: int) =
+  for i in 0 .. < x:
+    withLock counterLock:
+      var value = counter
+      value.inc()
+      counter = value
+
+spawn increment(10_000)
+spawn increment(10_000)
+sync()
+
+echo(counter)
