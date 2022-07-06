65ef4e0c52b657e429ee955581109634f5a1acf4
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-07-06 11:31:10 +0200

    [add] chapter7/Tweeter/src/database.nim

---
+++ chapter7/Tweeter/src/database.nim
@@ -0,0 +1,101 @@
+import times
+import db_sqlite
+import strutils
+
+type
+  Database* = ref object
+    db: DbConn
+
+  User* = object
+    username*: string
+    following*: seq[string]
+
+  Message* = object
+    username*: string
+    time*: Time
+    msg*: string
+
+# ------------------------------------------------------
+proc newDB*(filename: string = "Tweeter.db"): Database =
+  Database(db: open(filename, "", "", ""))
+
+# --------------------------------------
+proc post*(db: Database, msg: Message) =
+  if msg.msg.len() > 140:
+    raise newException(
+      ValueError,
+      "Message has to be less than 140 characteres."
+    )
+
+  db.db.exec(
+    sql"INSERT INTO Message VALUES (?, ?, ?);",
+    msg.username, $msg.time.toUnix, msg.msg
+  )
+
+# ------------------------------------------------------
+proc follow*(db: Database, follower: User, user: User) =
+  db.db.exec(
+    sql"INSERT INTO Following VALUES (?, ?);",
+    follower.username, user.username
+  )
+
+# --------------------------------------
+proc create*(db: Database, user: User) =
+  db.db.exec(
+    sql"INSERT INTO User VALUES (?);",
+    user.username
+  )
+
+# --------------------------------------------------------------------
+proc findUser*(db: Database, username: string, user: var User): bool =
+  let row = db.db.getRow(
+    sql"SELECT username FROM User WHERE username = ?;",
+    username
+  )
+
+  if row[0].len() == 0:
+    return false
+  else:
+    user.username = row[0]
+
+  let following = db.db.getAllRows(
+    sql"SELECT followed_user FROM Following WHERE follower = ?;",
+    username
+  )
+  user.following = @[]
+  for row in following:
+    if row[0].len() != 0:
+      user.following.add(row[0])
+
+  return true
+
+# ----------------------------------------
+proc findMessages*(db: Database,
+                   usernames: seq[string],
+                   limit: int = 10
+                  ): seq[Message] =
+  result = @[]
+
+  if usernames.len() == 0:
+    return
+
+  var whereClause = " WHERE "
+  for i in 0 .. < usernames.len():
+    whereClause.add("username = ? ")
+    if i != < usernames.len():
+      whereClause.add("or ")
+
+  let msgs = db.db.getAllRows(
+    sql(
+      "SELECT username, time, msg FROM Message" &
+      whereClause &
+      "ORDER BY time DESC LIMIT " & $limit
+    ),
+    usernames
+  )
+  for row in msgs:
+    result.add(Message(
+      username: row[0],
+      time: row[1].parseInt.fromUnix(),
+      msg: row[2]
+    ))
